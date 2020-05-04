084ac4ad1d28570db4b73c0d70406255761040fb
yettd <junkai.poh@gmail.com>
2020-05-04 16:26:12 +0800

    added a unity project

---
+++ 20_PA$_PohJunKai/Assets/Scripts/rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime;
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
+    }
+}
