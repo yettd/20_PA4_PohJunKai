6d15f4f816be1eeb0ccc098c100cdc5e9503085a
blasterscx <41160786+blasterscx@users.noreply.github.com>
2020-05-04 16:55:33 +0800

    updated
    weeee

---
+++ 20_PA4_PohJunKai/Assets/changescene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("loadscene", 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void loadscene()
+    {
+        SceneManager.LoadScene("cubescene");
+    }
+}
